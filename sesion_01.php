21f24b007bbaf93cd89953f0b5a58469416e1fee
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2019-05-06 07:29:56 -0500

    sesion_01

--- sesion_01.php
+++ sesion_01.php
@@ -1,3 +1,12 @@
 <?php
+$a="    Pacheco    ";
+$b="3";
+echo trim($a)."<br>";
+echo ltrim($a)."<br>";
+echo rtrim($a);
+echo strtolower($a);
+echo strtoupper($a);
+$parrafo="El que presenta a los derechos de el c. nombre bajo los estatutos de la ley en material penal, da como obligación al c. nombre";
+echo str_replace("nombre","Robin pachecho",$parrafo);
 
 ?>
