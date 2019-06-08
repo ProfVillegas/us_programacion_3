3d6f8d2d2c65a2f98b3e9854546c1a78106e6c3b
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2019-06-08 09:50:41 -0500

    Create s6_controlador.php

---
+++ s6_controlador.php
@@ -0,0 +1,12 @@
+<?php
+requiere_once("sesion_06.php");
+$args=$_REQUEST;
+$user= new Usuario();
+
+$respuesta= array();
+$respuesta['num_cliente']=$user->no_cliente;
+$respuesta['nombre_completo']=$user->nombre." ".$user->ap_paterno." ".$user->ap_materno;
+$respuesta['num_cuenta']=$user->cuenta->n_cuenta;
+
+echo json($respuesta,true);
+?>
