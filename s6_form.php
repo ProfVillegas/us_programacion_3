c7e67e2cea3ce85d6fc38af0ce161e5103b881d5
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2019-06-08 09:03:55 -0500

    Create s6_form.php

---
+++ s6_form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+        <meta http-equiv="X-UA-Compatible" content = "ie=edge">
+        <title> Crear cuentas </title>
+        
+    </head>
+    
+    <body>
+        <form id="CrearCuenta">
+            <table>
+                <tr>
+                    <td>Numero de cuenta</td><td>Crear</td>
+                </tr>
+                <tr> 
+                    <td><input type="text" name="n_cuenta"></td>
+                    <td><input type="button" value="enviar" name="enviar"></td>
+                </tr>
+            </table>
+        </form>
+        <table id="cuentas">
+        </table>
+    </body>
+</html>
